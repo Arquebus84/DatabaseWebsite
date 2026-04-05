9b928f7a966282c2bf406cf17a74eb14836dbc63
Arquebus84 <matteoventura06@gmail.com>
2026-04-05 19:58:52 -0400

    Initial Style for Nursery Website

--- public/index.js
+++ public/index.js
@@ -4,15 +4,25 @@
 // const {DatabaseSync} = require('node:sqlite');
 
 // // const table = document.getElementById("tableView");
-// // const patientBT = document.getElementById("patientBT");
+const patientBT = document.getElementById("patientBT");
 const facultyBT = document.getElementById('facultyBT');
+const familyBT = document.getElementById("familyBT");
+const paymentBT = document.getElementById("paymentBT");
 
 // // const tableValues = new DataAccess();
 // db = DatabaseSync('nurseryDB.db');
 //     // db = DatabaseSync('nurseryDB.db');
 // const rows = db.prepare('SELECT * FROM faculty').all();   //query.all()
-
-facultyBT.addEventListener('click', function(e){
-    document.getElementById("tableView").innerText = "Hello Table";//tableValues.getFacultyTable();
+patientBT.addEventListener('click', function(e){
+    document.getElementById("tableView").innerText = "Patient Table";//tableValues.getFacultyTable();
     // console.log(rows);
+});
+facultyBT.addEventListener('click', function(e){
+    document.getElementById("tableView").innerText = "Faculty Table";//tableValues.getFacultyTable();
+});
+familyBT.addEventListener('click', function(e){
+    document.getElementById("tableView").innerText = "Family Table";//tableValues.getFacultyTable();
+});
+paymentBT.addEventListener('click', function(e){
+    document.getElementById("tableView").innerText = "Payment Table";//tableValues.getFacultyTable();
 });
