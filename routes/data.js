9e1441ca3ad7084a1c1ed1ba4c69cc56754aacbc
Arquebus84 <matteoventura06@gmail.com>
2026-04-07 02:20:12 -0400

    Initial Commit For Express-Generator

---
+++ routes/data.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+// var db = require('./database');
+
+router.get('/', function(req, res, next){
+    // response.send('send all sample data');
+    res.render('example', {title: 'Express'});
+});
+
+router.get('/add', function(req, res, next){
+    // response.send('sample data');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-// const path = require('path');
-const express = require('express');
+var path = require('path');
+var express = require('express');
 // import express from 'express';
-const app = express();
-// const app = express();
+var app = express();
+
+var data = require('./routes/data');
+
+// var indexRouter = require('./routes/data');  //Fix this
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'views')));
+app.use('/', data);
 
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('views'));
 
 app.listen(PORT, ()=>{
     console.log(`App running on http://localhost:${PORT}`);
 });
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/public/index.html');
-});
+// app.get('/', (req, res)=>{
+//     res.sendFile(__dirname + '/public');
+// });
+
+// app.get('/', (req, res)=>{
+//     res.sendFile(__dirname + '/public/index.html');
+// });
+
+module.exports = app;
